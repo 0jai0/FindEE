50bcd4129a3578a2205aa9315d3ba22542a01562
0jai0 <88067577+0jai0@users.noreply.github.com>
2024-02-09 16:48:42 +0530

    upate 1

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import { google } from "googleapis";
 import path from "path";
 import stream from "stream";
 import 'dotenv/config'
+import mjAPI from'mathjax-node';
+import axios from "axios";
+
 
 const upload = multer();
 const app = express();
@@ -18,6 +21,14 @@ const db = new pg.Client({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
+/*const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "FindEE",
+  password: "Kiranmjv1027@",
+  port: 5432,
+});
+*/   
 
 db.connect();
 
@@ -51,21 +62,19 @@ app.get('/', async(req, res) => {
   } catch (error) {
     console.error('Error fetching data', error);
     res.status(500).send('Error fetching data');
-  }
+  } 
 });
-
 app.get('/search', async (req, res) => {
   const query = req.query.query.toLowerCase();
+  const generatedResponse_search = await generateResponse(query, "give the solution for the given question step by step:\n");
 
   try {
-
     const result = await db.query('SELECT * FROM solutions WHERE question_text ILIKE $1', [`%${query}%`]);
     const searchData = result.rows[0].solution_photo;
     //console.log(searchData);
-    res.render('entry.ejs', { result });
+    res.render('entry.ejs', { result, generatedResponse_search});
   } catch (err) {
-    console.error('Error executing query', err);
-    res.status(500).send('Question was not avalible Please upload your question in home page ');
+    res.render('entry.ejs', {generatedResponse_search, query});
   }
 });
 
@@ -79,56 +88,76 @@ const auth = new google.auth.GoogleAuth({
 
 
 
-app.post("/upload", upload.any(), async (req, res) => {
-    try {
-        const { body, files } = req;
+
+app.post('/upload', upload.any(), async (req, res) => {
+  try {
+    const { body, files } = req;
+    
+    var data;
+    for (let f = 0; f < files.length; f += 1) {
+        const selectedElements = body.selectedElements || [];
+        const subject_no = await db.query('SELECT * FROM subjects WHERE subject LIKE $1', [`%${selectedElements}%`]);
+        data = await uploadFile( subject_no.rows[0].subject_id, files[f]);
         
-        
-        for (let f = 0; f < files.length; f += 1) {
-            const selectedElements = body.selectedElements || [];
-            const subject_no = await db.query('SELECT * FROM subjects WHERE subject LIKE $1', [`%${selectedElements}%`]);
-            await uploadFile( subject_no.rows[0].subject_id, files[f]);
-            
-        }
-        res.status(200).send("Form Submitted");
-    } catch (f) {
-        res.send(f.message);
     }
+    const query = await generateContent('https://drive.google.com/uc?id='+data, "convert this image into text:\n");
+    console.log(query);
+    const generatedResponse_search = await generateContent('https://drive.google.com/uc?id='+data, "give the solution for the given question step by step:\n");
+    res.render('entry.ejs', {generatedResponse_search, query});
+  } catch (err) {
+    res.status(500).send('Error updating the database');
+  }
 });
 
 
+
+//const stream = require('stream'); // Ensure 'stream' module is imported
+
 const uploadFile = async (bodyObject, fileObject) => {
   try {
-      const bufferStream = new stream.PassThrough();
-      bufferStream.end(fileObject.buffer);
-      const { data } = await google.drive({ version: "v3", auth }).files.create({
-          media: {
-              mimeType: fileObject.mimeType,
-              body: bufferStream,
-          },
-          requestBody: {
-              name: fileObject.originalname,
-              parents: ["1P2eZAYUgIZEqD4PnPZ9vfmwgDyA_8ks0"], 
-          },
-          fields: "id,name",
-      });
-      console.log(`Uploaded file ${data.name} ${data.id}`);
-      (async () => {
-        const worker = await createWorker('eng');
-        const link = 'https://drive.google.com/uc?id='+ data.id;
-        const ret = await worker.recognize(link);
-        await db.query('INSERT INTO solutions (subject_id, question_type, question_photo,question_text) VALUES ($1, $2, $3, $4)', [bodyObject, 'NPY', data.id,ret.data.text.toLowerCase()]);
-        console.log(ret.data.text);
-        await worker.terminate();
-      })();
+    // Create a readable stream from the buffer
+    const bufferStream = new stream.PassThrough();
+    bufferStream.end(fileObject.buffer);
+
+    // Upload file to Google Drive
+    const { data } = await google.drive({ version: "v3", auth }).files.create({
+      media: {
+        mimeType: fileObject.mimeType,
+        body: bufferStream,
+      },
+      requestBody: {
+        name: fileObject.originalname,
+        parents: ["1P2eZAYUgIZEqD4PnPZ9vfmwgDyA_8ks0"],
+      },
+      fields: "id,name",
+    });
+
+    console.log(`Uploaded file ${data.name} ${data.id}`);
+
+    // Process the uploaded file
+    (async () => {
+      
+
+      // Handle image generation or any other processing
+      const generatedResponse = await generateContent('https://drive.google.com/uc?id='+data.id, "convert this image into text:\n");
+
+      console.log(generatedResponse);
+
+      // Insert data into the database
+      await db.query('INSERT INTO solutions (subject_id, question_type, question_photo,question_text) VALUES ($1, $2, $3, $4)', [bodyObject, 'NPY', data.id, generatedResponse.toLowerCase()]);
+    })();
+    return data.id;
   } catch (error) {
-      console.error("Error uploading file:", error);
-      throw new Error(`Failed to upload file: ${error.message}`);
+    console.error("Error uploading file:", error);
+    throw new Error(`Failed to upload file: ${error.message}`);
   }
 };
 
+
+
 app.get('/entry/:subject', async (req, res) => {
   const subject = req.params.subject;
+  //console.log(subject);
   const subject_code = await db.query('SELECT * FROM subjects WHERE subject = ($1)', [subject]);
   const subjectId = subject_code.rows[0].subject_id;
   const result =  await db.query('SELECT * FROM solutions WHERE subject_id = ($1)', [subjectId]);
@@ -168,7 +197,7 @@ app.post('/uploadImage', upload.any(), async (req, res) => {
   const imageFile = req.files[0]; 
   try {
     await uploadFile1(questionId, imageFile); 
-
+    res.render('kk.ejs');
   } catch (err) {
     res.status(500).send('Error updating the database');
   }
@@ -225,7 +254,131 @@ app.post('/addSubject', async (req, res) => {
 
 
 
-app.listen(port, () => {
+// using  gemini api 
+
+import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+//import 'dotenv/config'
+const MODEL_NAME = "gemini-pro";
+const API_KEY = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(API_KEY);
+async function generateResponse(inputText) {
+  
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+
+  const generationConfig = {
+    temperature: 0.9,
+    topK: 1,
+    topP: 1,
+    maxOutputTokens: 2048,
+  };
+
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ];
+
+  const parts = [{ text: inputText }];
+
+  const result = await model.generateContent({
+    contents: [{ role: "user", parts }],
+    generationConfig,
+    safetySettings,
+  });
+
+  const response = result.response;
+  return response.text();
+}
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/process', async (req, res) => {
+  const { inputText, questionText, index } = req.body;
+  //console.log(inputText);
+  const result =  await db.query('SELECT * FROM solutions WHERE subject_id = ($1)', [inputText]);
+  const output =  await db.query('SELECT * FROM solutions WHERE question_id = ($1)', [questionText]);
+  const inputTextFromDatabase = output.rows[0].question_photo;
+  //console.log(inputTextFromDatabase);
+  try {
+    const generatedResponse = await generateContent('https://drive.google.com/uc?id='+inputTextFromDatabase, "give the solution in for the image step by step:\n");
+    //console.log('https://drive.google.com/uc?id='+inputTextFromDatabase)
+    //console.log(generatedResponse);
+    res.render('entry.ejs', { result, generatedResponse, index });
+    } catch (err) {
+    console.error('Error generating response:', err);
+    res.status(500).json({ error: 'Error generating response' });
+  }
+}); 
+ 
+
+
+
+
+//image ai
+
+//const fs = require("fs");
+import { promises as fs } from "fs";
+// Access your API key as an environment variable (see "Set up your API key" above)
+const generationConfig = { temperature: 0.4, topP: 1, topK: 32, maxOutputTokens: 4096 };
+
+// 2. Initialise Model for vision-based generation
+const model = genAI.getGenerativeModel({ model: "gemini-pro-vision", generationConfig });
+
+// 3. Generate Content with Image Input
+async function generateContent(file, text) {
+  try {
+    // Read image data
+    const imageData = await axios.get(file, {
+      responseType: 'arraybuffer'
+    });
+    const imageBase64 = Buffer.from(imageData.data, 'binary').toString('base64');
+
+  
+    // Define parts
+    const parts = [
+      { text: text },
+      {
+        inlineData: {
+          mimeType: "image/png",
+          //mineType: "image/jpg",
+          data: imageBase64
+        } 
+      },
+    ];
+
+    // Generate content using both text and image input
+    const result = await model.generateContent({ contents: [{ role: "user", parts }] });
+    const response = await result.response;
+    //console.log(response.text());
+    return response.text();
+  } catch (error) {
+    console.error('Error generating content:', error.message);
+    throw new Error('Error generating content1:', error);
+  }
+}
+
+
+//const imagePath = "https://drive.google.com/uc?id=1fEn6VEtoYat0XDUUheqVQQEt2vWSZiee"; // Path to the uploaded image
+
+// Generate content using the uploaded image
+//generateContent();
+
+
+
+
+app.listen(port, () => { 
   console.log(`Server running on http://localhost:${port}`);
 });
 
